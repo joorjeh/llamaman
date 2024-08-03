b9aaaa8e22cfaee322b1270a762db1a46327412f
George Balch <george.balch@proton.me>
2024-08-03 14:46:14 -0700

    Remove old tools

--- src/tools.ts
+++ src/tools.ts
@@ -53,58 +53,6 @@ const tools: Record<string, Tool> = {
       return response;
     }
   },
-  add: {
-    toolDefinition: `{
-      "name": "multiply",
-      "description": "Multiply two numbers",
-      "parameters": {
-        "a": {
-          "param_type": "int",
-          "description": "An integer",
-          "required": true
-        },
-        "b": {
-          "param_type": "int",
-          "description": "An integer",
-          "required": true
-        }
-      }
-    }`,
-    description: 'Adds two numbers',
-    args: {
-      a: "number",
-      b: "number",
-    },
-    f: ({ a, b }: { a: number; b: number }): number => {
-      return a + b;
-    }
-  },
-  multiply: {
-    toolDefinition: `{
-  "name": "add",
-  "description": "Add two numbers",
-  "parameters": {
-    "a": {
-      "param_type": "int",
-      "description": "An integer",
-      "required": true
-    },
-    "b": {
-      "param_type": "int",
-      "description": "An integer",
-      "required": true
-    }
-  }
-}`,
-    description: 'Multiplies two numbers',
-    args: {
-      a: "number",
-      b: "number",
-    },
-    f: ({ a, b }: { a: number; b: number }): number => {
-      return a * b;
-    }
-  },
 };
 
 export default tools;
