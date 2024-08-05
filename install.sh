5f8aed5ed4de40ddc89cfdd214329334d9433baa
George Balch <george.balch@proton.me>
2024-08-04 17:04:40 -0700

    Update install script to define vogelsang program as bash script that rebuilds each time program is started, in order to load new tools

--- install.sh
+++ install.sh
@@ -7,3 +7,32 @@ fi
 
 yarn
 yarn tauri build
+
+if [ ! -d "bin" ]; then
+  mkdir bin
+fi
+
+if [ ! -f bin/vogelsang ]; then
+  touch bin/vogelsang
+  chmod +x bin/vogelsang
+fi
+
+cat <<'EOF' >bin/vogelsang
+#!/bin/bash
+
+cd $HOME/.local/vogelsang/
+bash ./load_tools.sh
+
+yarn 
+yarn tauri build
+
+./src-tauri/target/release/bundle/appimage/vogelsang_*.AppImage
+
+EOF
+
+echo '' >>$HOME/.bashrc
+echo '# Add Vogelsang binary directory to PATH' >>$HOME/.bashrc
+echo 'export PATH=$PATH:$HOME/.local/vogelsang/bin' >>$HOME/.bashrc
+
+# Reload .bashrc
+source $HOME/.bashrc
