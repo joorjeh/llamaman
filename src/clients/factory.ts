383af35e85e8b4f1a1798bbab7b180b540909c85
George Balch <george.balch@proton.me>
2024-08-07 21:18:22 -0700

    Remove dead import

--- src/clients/factory.ts
+++ src/clients/factory.ts
@@ -1,47 +1,46 @@
 import AwsClient from "./AwsClient";
 import StreamingClient from "./Client";
 import OllamaClient from "./OllamaClient";
-import OpenRouterClient from "./OpenRouterClient";
 
 export interface StreamingClientOptions {
-    region?: string,
-    model?: string,
-    temperature?: number,
-    top_p?: number,
-    url?: string,
+  region?: string,
+  model?: string,
+  temperature?: number,
+  top_p?: number,
+  url?: string,
 }
 
 export const getStreamingClient = async ({
-    platform,
-    options,
+  platform,
+  options,
 }: {
-    platform: string,
-    options: StreamingClientOptions,
+  platform: string,
+  options: StreamingClientOptions,
 }): Promise<StreamingClient> => {
-    // Parse options based on platform
-    if (platform === 'aws') {
-        return await AwsClient.create({
-            region: options.region,
-            model: options.model,
-            temperature: options.temperature,
-            top_p: options.top_p,
-        });
-    } else if (platform === 'ollama') {
-        return OllamaClient.create({
-            url: options.url,
-            temperature: options.temperature,
-            top_p: options.top_p,
-            model: options.model,
-        });
-    } 
-    // else if (platform === 'openrouter') {
-    //     return OpenRouterClient.create({
-    //         temperature: options.temperature,
-    //         top_p: options.top_p,
-    //         model: options.model, 
-    //     });
-    // }
-    // This should never be reached, simply
-    // to satisfy TypeScript compiler
-    return OllamaClient.create(options);
+  // Parse options based on platform
+  if (platform === 'aws') {
+    return await AwsClient.create({
+      region: options.region,
+      model: options.model,
+      temperature: options.temperature,
+      top_p: options.top_p,
+    });
+  } else if (platform === 'ollama') {
+    return OllamaClient.create({
+      url: options.url,
+      temperature: options.temperature,
+      top_p: options.top_p,
+      model: options.model,
+    });
+  }
+  // else if (platform === 'openrouter') {
+  //     return OpenRouterClient.create({
+  //         temperature: options.temperature,
+  //         top_p: options.top_p,
+  //         model: options.model, 
+  //     });
+  // }
+  // This should never be reached, simply
+  // to satisfy TypeScript compiler
+  return OllamaClient.create(options);
 }
